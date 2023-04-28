e19bb1086abc86569650aa299acc2da2c4974a5f
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 18:03:55 +0530

    initial commit

---
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/LookUpType.java
@@ -0,0 +1,45 @@
+package com.sa.fund.master.lookupcontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "lookup_type")
+public class LookUpType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String code;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
