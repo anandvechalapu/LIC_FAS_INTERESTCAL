cad13ee589ea35476979565a519437e36d679479
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:25:53 +0530

    initial commit

---
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/model/.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sa.fund.master.fundmastercontroller.model.FundMgmtChargesSlabMstEntity;
+
+public interface FmcsearchFmcInterestRateSlabRepository extends JpaRepository<FundMgmtChargesSlabMstEntity, Long> {
+
+}
