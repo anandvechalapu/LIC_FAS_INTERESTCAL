de4343506ed26e7f054eb06811d5843be05b25b1
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 18:01:58 +0530

    initial commit

---
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/MstPageLookUpValue.java
@@ -0,0 +1,59 @@
+package com.sa.fund.master.lookupcontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MstPageLookUpValue")
+public class MstPageLookUpValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String code;
+    private String description;
+    private String value;
+    private boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
