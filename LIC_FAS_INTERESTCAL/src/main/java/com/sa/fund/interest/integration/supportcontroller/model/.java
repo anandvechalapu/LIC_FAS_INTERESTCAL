d3a3cb57299363cf6a58d9c118484a9fe33d4700
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 16:09:27 +0530

    initial commit

---
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/supportcontroller/model/.java
@@ -0,0 +1,16 @@
+atchStatus() {
+        return batchStatus;
+    }
+
+    public void setBatchStatus(String batchStatus) {
+        this.batchStatus = batchStatus;
+    }
+
+    public String getBatchNumber() {
+        return batchNumber;
+    }
+
+    public void setBatchNumber(String batchNumber) {
+        this.batchNumber = batchNumber;
+    }
+}
