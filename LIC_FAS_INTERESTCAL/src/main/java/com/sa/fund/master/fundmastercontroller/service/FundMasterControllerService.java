3ffec6ff0f3a1d94c7561d493a1235fc1f20a946
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:34:03 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/service/FundMasterControllerService.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/service/FundMasterControllerService.java
@@ -1,10 +1,11 @@
 package com.sa.fund.master.fundmastercontroller.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sa.fund.master.fundmastercontroller.dto.CommonResponseDto;
-import com.sa.fund.master.fundmastercontroller.dto.FundInterestSlabMstDto;
+import com.sa.fund.master.fundmastercontroller.model.AirMfrRa;
 import com.sa.fund.master.fundmastercontroller.repository.FundMasterControllerRepository;
 
 @Service
@@ -13,7 +14,8 @@ public class FundMasterControllerService {
     @Autowired
     private FundMasterControllerRepository fundMasterControllerRepository;
 
-    public CommonResponseDto<Object> saveInterestRateSlab(FundInterestSlabMstDto dto) throws IllegalArgumentException {
-        return fundMasterControllerRepository.saveInterestRateSlab(dto);
+    public List<AirMfrRa> getAirMfrRa() {
+        return fundMasterControllerRepository.getAirMfrRa();
     }
+
 }
