b4526096e380b209c6c39d76332fb486ab477e17
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:05:16 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/controller/FundMasterController.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/controller/FundMasterController.java
@@ -1,25 +1,29 @@
 package com.sa.fund.master.fundmastercontroller.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.sa.fund.master.fundmastercontroller.model.FundInterestRateSlabMst;
+import com.sa.fund.master.fundmastercontroller.service.FundMasterService;
 import com.sa.fund.master.fundmastercontroller.dto.CommonResponseDto;
-import com.sa.fund.master.fundmastercontroller.dto.FundInterestSlabMstDto;
-import com.sa.fund.master.fundmastercontroller.service.FundMasterControllerService;
 
 @RestController
 @RequestMapping("/fundmaster")
 public class FundMasterController {
-
+    
     @Autowired
-    private FundMasterControllerService fundMasterControllerService;
-
-    @PostMapping("/saveInterestRateSlab")
-    public CommonResponseDto<Object> saveInterestRateSlab(@RequestBody FundInterestSlabMstDto dto) throws IllegalArgumentException {
-        return fundMasterControllerService.saveInterestRateSlab(dto);
+    FundMasterService fundMasterService;
+    
+    // Method to save the interest rate slab
+    @PostMapping(value = "/saveFmcInterestRateSlab")
+    public ResponseEntity<Object> saveFmcInterestRateSlab(@RequestBody FundInterestRateSlabMst fundInterestRateSlabMst) {
+        FundInterestRateSlabMst validateRequestBody = fundMasterService.validateRequestBody(fundInterestRateSlabMst);
+        CommonResponseDto<Object> response = fundMasterService.callSaveFmcInterestRateSlab(validateRequestBody);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
+    
 }
