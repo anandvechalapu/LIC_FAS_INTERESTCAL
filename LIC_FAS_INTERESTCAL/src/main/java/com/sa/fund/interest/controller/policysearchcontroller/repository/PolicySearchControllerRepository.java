f93396cb481fab5c229963b52b8cdc5d4329ca1c
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 14:10:33 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/repository/PolicySearchControllerRepository.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/repository/PolicySearchControllerRepository.java
@@ -1,29 +1,11 @@
 package com.sa.fund.interest.controller.policysearchcontroller.repository;
 
-import com.sa.fund.interest.controller.policysearchcontroller.dto.AccountsDto;
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonRequestDto;
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonResponseDto;
-import com.sa.fund.interest.controller.policysearchcontroller.entity.PolicyFundHistoryV2Entity;
-import com.sa.fund.interest.controller.policysearchcontroller.entity.PolicyFundStmtV2Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+@Repository
+public interface PolicySearchControllerRepository extends JpaRepository<DebitFundHistory, Long> {
 
-public interface PolicySearchControllerRepository extends JpaRepository<PolicyFundHistoryV2Entity, Long> {
-    /**
-     * Get a list of PolicyFundHistoryV2Entity objects ordered by createdOn and ID in ascending order
-     *
-     * @param commonRequestDto The optional payload query parameter in the request
-     * @return List of PolicyFundHistoryV2Entity objects
-     */
-    List<PolicyFundHistoryV2Entity> getAllPolicyFundHistoryV2Entities(CommonRequestDto commonRequestDto);
+    public DebitFundHistory getDebitFundHistoryByPolicy(Long policyNo);
 
-    /**
-     * Get a list of AccountsDto objects from a list of PolicyFundHistoryV2Entity objects
-     *
-     * @param policyFundHistoryV2Entities List of PolicyFundHistoryV2Entity objects
-     * @param policyFundStmtV2Entity      The corresponding PolicyFundStmtV2Entity object in the database
-     * @return List of AccountsDto objects
-     */
-    List<AccountsDto> getAllAccountsDtos(List<PolicyFundHistoryV2Entity> policyFundHistoryV2Entities, PolicyFundStmtV2Entity policyFundStmtV2Entity);
 }
