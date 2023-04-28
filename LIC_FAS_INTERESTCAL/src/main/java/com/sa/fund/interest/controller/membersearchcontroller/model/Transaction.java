bd6751fc4aa42897bf076f142cf9cd39cac78588
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 12:43:52 +0530

    initial commit

---
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/membersearchcontroller/model/Transaction.java
@@ -0,0 +1,41 @@
+package com.sa.fund.interest.controller.membersearchcontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String policyNumber;
+    private String memberId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+}
