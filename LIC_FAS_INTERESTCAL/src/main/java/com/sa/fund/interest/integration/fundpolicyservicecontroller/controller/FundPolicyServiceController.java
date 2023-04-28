40a56ba9d798009c4038fc9e075369fe46854136
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 15:30:54 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/fundpolicyservicecontroller/controller/FundPolicyServiceController.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/fundpolicyservicecontroller/controller/FundPolicyServiceController.java
@@ -1,30 +1,18 @@
 package com.sa.fund.interest.integration.fundpolicyservicecontroller.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sa.fund.interest.integration.fundpolicyservicecontroller.model.FundChangeDto;
-import com.sa.fund.interest.integration.fundpolicyservicecontroller.model.CommonResponseDto;
-import com.sa.fund.interest.integration.fundpolicyservicecontroller.model.FundPolicyService;
-import com.sa.fund.interest.integration.fundpolicyservicecontroller.service.FundPolicyServiceControllerService;
+import com.sa.fund.interest.service.FundPolicyService;
 
 @RestController
 public class FundPolicyServiceController {
+	
+	private final FundPolicyService fundPolicyService;
+	
+	public FundPolicyServiceController(FundPolicyService fundPolicyService) {
+		this.fundPolicyService = fundPolicyService;
+	}
+	
+	//Add controller methods here
 
-    @Autowired
-    private FundPolicyServiceControllerService service;
-
-    @PostMapping("/process_policy")
-    public CommonResponseDto processPolicy(@RequestBody FundChangeDto fundChangeDto, @RequestParam String serviceId) {
-        return service.processPolicy(fundChangeDto, serviceId);
-    }
-
-    @GetMapping("/find_policy_service")
-    public FundPolicyService findPolicyServiceById(@RequestParam String serviceId) {
-        return service.findPolicyServiceById(serviceId);
-    }
 }
