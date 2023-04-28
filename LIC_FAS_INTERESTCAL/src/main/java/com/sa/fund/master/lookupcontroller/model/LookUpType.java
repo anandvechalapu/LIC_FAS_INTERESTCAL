029b767e55bec21f8fe74d954ada2ced76fc1547
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 18:07:54 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/LookUpType.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/LookUpType.java
@@ -1,23 +1,27 @@
 package com.sa.fund.master.lookupcontroller.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "lookup_type")
 public class LookUpType {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "code")
     private String code;
-    private String description;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "status")
+    private String status;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "lookUpType")
+    private List<LookUpValue> lookUpValues;
 
     public Long getId() {
         return id;
@@ -35,11 +39,27 @@ public class LookUpType {
         this.code = code;
     }
 
-    public String getDescription() {
-        return description;
+    public String getName() {
+        return name;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<LookUpValue> getLookUpValues() {
+        return lookUpValues;
+    }
+
+    public void setLookUpValues(List<LookUpValue> lookUpValues) {
+        this.lookUpValues = lookUpValues;
     }
 }
