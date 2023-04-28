28f24aea8fd37750f7caf72aace457fe6f163fa2
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:32:21 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/repository/FundMasterControllerRepository.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/repository/FundMasterControllerRepository.java
@@ -1,19 +1,17 @@
 package com.sa.fund.master.fundmastercontroller.repository;
 
-import com.sa.fund.master.fundmastercontroller.dto.CommonResponseDto;
-import com.sa.fund.master.fundmastercontroller.dto.FundInterestSlabMstDto;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FundMasterControllerRepository extends JpaRepository<FundInterestSlabMstDto, Long> {
+import com.sa.fund.master.fundmastercontroller.model.AirMfrRa;
 
-    /**
-     * Method to save an interest rate slab for the funds
-     * 
-     * @param dto
-     * @return CommonResponseDto object with the appropriate status and message
-     * @throws IllegalArgumentException if any of the mandatory fields are missing or invalid
-     */
-    public CommonResponseDto<Object> saveInterestRateSlab(FundInterestSlabMstDto dto) throws IllegalArgumentException;
+@Repository
+public interface FundMasterControllerRepository extends JpaRepository<AirMfrRa, Long> {
+
+    @Query("SELECT a FROM AirMfrRa a")
+    List<AirMfrRa> getAirMfrRa();
+
 }
