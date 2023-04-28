f46a356ce8fa784391c5897244e5a35f8b6dd558
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 14:02:55 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/service/PolicySearchControllerService.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/service/PolicySearchControllerService.java
@@ -1,33 +1,30 @@
 package com.sa.fund.interest.controller.policysearchcontroller.service;
 
-import com.sa.fund.interest.controller.policysearchcontroller.dto.AccountsDto;
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonRequestDto;
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonResponseDto;
-import com.sa.fund.interest.controller.policysearchcontroller.entity.PolicyFundHistoryV2Entity;
-import com.sa.fund.interest.controller.policysearchcontroller.entity.PolicyFundStmtV2Entity;
-import com.sa.fund.interest.controller.policysearchcontroller.repository.PolicySearchControllerRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.sa.fund.interest.controller.policysearchcontroller.repository.PolicySearchControllerRepository;
 
 @Service
 public class PolicySearchControllerService {
 
-    @Autowired
     private PolicySearchControllerRepository policySearchControllerRepository;
 
-    public CommonResponseDto getAllPolicyFundHistoryV2Entities(CommonRequestDto commonRequestDto) {
-        List<PolicyFundHistoryV2Entity> policyFundHistoryV2Entities = policySearchControllerRepository.getAllPolicyFundHistoryV2Entities(commonRequestDto);
-        List<AccountsDto> accountsDtos = new ArrayList<>();
-        for (PolicyFundHistoryV2Entity policyFundHistoryV2Entity : policyFundHistoryV2Entities) {
-            PolicyFundStmtV2Entity policyFundStmtV2Entity = policyFundHistoryV2Entity.getPolicyFundStmtV2Entity();
-            AccountsDto accountsDto = policySearchControllerRepository.getAllAccountsDtos(policyFundHistoryV2Entities, policyFundStmtV2Entity).get(0);
-            accountsDtos.add(accountsDto);
-        }
-        CommonResponseDto commonResponseDto = new CommonResponseDto();
-        commonResponseDto.setAccountsDtos(accountsDtos);
-        return commonResponseDto;
+    @Autowired
+    public PolicySearchControllerService(PolicySearchControllerRepository policySearchControllerRepository) {
+        this.policySearchControllerRepository = policySearchControllerRepository;
     }
+
+    public ResponseEntity<?> searchByPolicyNo(String policyNumber) {
+        return policySearchControllerRepository.searchByPolicyNo(policyNumber);
+    }
+
 }
