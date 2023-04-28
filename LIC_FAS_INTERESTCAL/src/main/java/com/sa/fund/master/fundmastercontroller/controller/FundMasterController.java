f9a8a3d21dc40ac9e9b34558436b7f3f7df6ef79
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:35:31 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/controller/FundMasterController.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/controller/FundMasterController.java
@@ -1,29 +1,43 @@
+package com.sa.fund.master.fundmastercontroller.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.sa.fund.master.fundmastercontroller.model.AirMfrRa;
+
+@Repository
+public interface FundMasterControllerRepository extends JpaRepository<AirMfrRa, Long> {
+
+    @Query("SELECT a FROM AirMfrRa a")
+    List<AirMfrRa> getAirMfrRa();
+
+}
+
 package com.sa.fund.master.fundmastercontroller.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.sa.fund.master.fundmastercontroller.model.FundInterestRateSlabMst;
-import com.sa.fund.master.fundmastercontroller.service.FundMasterService;
-import com.sa.fund.master.fundmastercontroller.dto.CommonResponseDto;
+
+import com.sa.fund.master.fundmastercontroller.model.AirMfrRa;
+import com.sa.fund.master.fundmastercontroller.service.FundMasterControllerService;
 
 @RestController
-@RequestMapping("/fundmaster")
+@RequestMapping("/api/fundmastercontroller")
 public class FundMasterController {
-    
+
     @Autowired
-    FundMasterService fundMasterService;
-    
-    // Method to save the interest rate slab
-    @PostMapping(value = "/saveFmcInterestRateSlab")
-    public ResponseEntity<Object> saveFmcInterestRateSlab(@RequestBody FundInterestRateSlabMst fundInterestRateSlabMst) {
-        FundInterestRateSlabMst validateRequestBody = fundMasterService.validateRequestBody(fundInterestRateSlabMst);
-        CommonResponseDto<Object> response = fundMasterService.callSaveFmcInterestRateSlab(validateRequestBody);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    private FundMasterControllerService fundMasterControllerService;
+
+    @GetMapping("/airmfrra")
+    public List<AirMfrRa> getAirMfrRa() {
+        return fundMasterControllerService.getAirMfrRa();
     }
-    
+
 }
