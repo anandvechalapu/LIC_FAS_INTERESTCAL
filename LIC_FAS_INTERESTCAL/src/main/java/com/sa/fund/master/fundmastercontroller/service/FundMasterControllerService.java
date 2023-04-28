225097cd08241548aea0cf7cc814284405bf99fe
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:45:42 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/service/FundMasterControllerService.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/fundmastercontroller/service/FundMasterControllerService.java
@@ -1,21 +1,36 @@
 package com.sa.fund.master.fundmastercontroller.service;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
-import com.sa.fund.master.fundmastercontroller.model.AirMfrRa;
+import com.sa.fund.master.fundmastercontroller.model.Variant;
 import com.sa.fund.master.fundmastercontroller.repository.FundMasterControllerRepository;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Service
+@Timed
 public class FundMasterControllerService {
 
-    @Autowired
-    private FundMasterControllerRepository fundMasterControllerRepository;
+	@Autowired
+	private FundMasterControllerRepository fundMasterControllerRepository;
+	
+	private final Counter counter;
 
-    public List<AirMfrRa> getAirMfrRa() {
-        return fundMasterControllerRepository.getAirMfrRa();
-    }
+	public FundMasterControllerService(MeterRegistry meterRegistry) {
+		this.counter = meterRegistry.counter("fund.master.controller");
+	}
 
+	@Cacheable(unless = "#result == null")
+	public Optional<Variant> getVariant(Long variantId) {
+		counter.increment();
+		return fundMasterControllerRepository.getVariant(variantId);
+	}
+	
 }
