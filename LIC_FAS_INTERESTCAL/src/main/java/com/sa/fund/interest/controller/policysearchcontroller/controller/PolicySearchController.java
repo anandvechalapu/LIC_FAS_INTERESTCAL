0512db20fbe6dc899a31c38d56713625d7613de4
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 14:11:26 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/controller/PolicySearchController.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/controller/PolicySearchController.java
@@ -1,24 +1,39 @@
+package com.sa.fund.interest.controller.policysearchcontroller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.sa.fund.interest.domain.DebitFundHistory;
+
+@Repository
+public interface PolicySearchControllerRepository extends JpaRepository<DebitFundHistory, Long> {
+
+    @Query("SELECT d FROM DebitFundHistory d where policyNo = ?1")
+    DebitFundHistory getDebitFundHistoryByPolicy(Long policyNo);
+}
+
 package com.sa.fund.interest.controller.policysearchcontroller.controller;
 
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonRequestDto;
-import com.sa.fund.interest.controller.policysearchcontroller.dto.CommonResponseDto;
-import com.sa.fund.interest.controller.policysearchcontroller.service.PolicySearchControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sa.fund.interest.controller.policysearchcontroller.model.DebitFundHistory;
+import com.sa.fund.interest.controller.policysearchcontroller.service.PolicySearchControllerService;
+
 @RestController
-@RequestMapping("/fund/interest/policy/search")
+@RequestMapping("/api/policysearchcontroller")
 public class PolicySearchController {
 
     @Autowired
     private PolicySearchControllerService policySearchControllerService;
 
-    @PostMapping
-    public CommonResponseDto getAllPolicyFundHistoryV2Entities(@RequestBody CommonRequestDto commonRequestDto) {
-        return policySearchControllerService.getAllPolicyFundHistoryV2Entities(commonRequestDto);
+    @GetMapping("/debitfundhistory/{policyNo}")
+    public DebitFundHistory getDebitFundHistoryByPolicy(@PathVariable Long policyNo) {
+        return policySearchControllerService.getDebitFundHistoryByPolicy(policyNo);
     }
 
 }
