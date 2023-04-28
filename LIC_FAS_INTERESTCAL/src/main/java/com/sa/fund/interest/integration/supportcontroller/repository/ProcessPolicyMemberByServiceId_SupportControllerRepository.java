dbcf981457a5bb0dc84f2cfcf43d4fae8949f1e0
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 16:00:54 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/supportcontroller/repository/ProcessPolicyMemberByServiceId_SupportControllerRepository.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/supportcontroller/repository/ProcessPolicyMemberByServiceId_SupportControllerRepository.java
@@ -1,10 +1,11 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sa.fund.interest.integration.supportcontroller.dto.CommonResponseDto;
-import com.sa.fund.interest.integration.supportcontroller.exception.RequiredFieldException;
-
-public interface ProcessPolicyMemberByServiceId_SupportControllerRepository extends JpaRepository<Long, Long> {
-	
-	CommonResponseDto<Map<String, String>> processMemberFundByPolicy(Long serviceId) throws RequiredFieldException, ApplicationException;
+@Repository
+public interface ProcessPolicyMemberByServiceId_SupportControllerRepository extends JpaRepository<FundChangeDto, String> {
+    @Modifying
+    @Query("UPDATE FundChangeDto SET service_id = :service_id, source_policy_id = :source_policy_id, destination_policy_id = :destination_policy_id WHERE id = :id")
+    int updateFundChangeDto(@Param("service_id") String service_id, @Param("source_policy_id") String source_policy_id, 
+    @Param("destination_policy_id") String destination_policy_id, @Param("id") String id);
     
+    @Query("SELECT f FROM FundChangeDto f WHERE service_id = :service_id OR (source_policy_id = :source_policy_id AND destination_policy_id = :destination_policy_id)")
+    List<FundChangeDto> findByServiceIdOrSourceAndDestinationPolicies(@Param("service_id") String service_id, 
+    @Param("source_policy_id") String source_policy_id, @Param("destination_policy_id") String destination_policy_id);
 }
