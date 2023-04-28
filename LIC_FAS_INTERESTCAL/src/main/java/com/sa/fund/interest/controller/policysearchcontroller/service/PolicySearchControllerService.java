8d69069d1755fbb1299974fc2c82d405d234615f
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 14:11:03 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/service/PolicySearchControllerService.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/controller/policysearchcontroller/service/PolicySearchControllerService.java
@@ -1,30 +1,19 @@
 package com.sa.fund.interest.controller.policysearchcontroller.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.persistence.EntityManager;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.sa.fund.interest.controller.policysearchcontroller.repository.PolicySearchControllerRepository;
+import com.sa.fund.interest.domain.DebitFundHistory;
 
 @Service
 public class PolicySearchControllerService {
 
+    @Autowired
     private PolicySearchControllerRepository policySearchControllerRepository;
 
-    @Autowired
-    public PolicySearchControllerService(PolicySearchControllerRepository policySearchControllerRepository) {
-        this.policySearchControllerRepository = policySearchControllerRepository;
-    }
-
-    public ResponseEntity<?> searchByPolicyNo(String policyNumber) {
-        return policySearchControllerRepository.searchByPolicyNo(policyNumber);
+    public DebitFundHistory getDebitFundHistoryByPolicy(Long policyNo) {
+        return policySearchControllerRepository.getDebitFundHistoryByPolicy(policyNo);
     }
 
 }
