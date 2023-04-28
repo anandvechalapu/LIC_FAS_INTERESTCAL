0c7a40691127d660148b4054926c81b703bd4122
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 17:57:59 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/MstLookUpTypesEntity.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/master/lookupcontroller/model/MstLookUpTypesEntity.java
@@ -1,9 +1,32 @@
 package com.sa.fund.master.lookupcontroller.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mst_look_up_types")
 public class MstLookUpTypesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
     private String name;
-    private String status;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_active")
+    private boolean isActive;
 
     public Long getId() {
         return id;
@@ -11,6 +34,14 @@ public class MstLookUpTypesEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public String getName() {
@@ -21,11 +52,20 @@ public class MstLookUpTypesEntity {
         this.name = name;
     }
 
-    public String getStatus() {
-        return status;
+    public String getDescription() {
+        return description;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setDescription(String description) {
+        this.description = description;
     }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 }
