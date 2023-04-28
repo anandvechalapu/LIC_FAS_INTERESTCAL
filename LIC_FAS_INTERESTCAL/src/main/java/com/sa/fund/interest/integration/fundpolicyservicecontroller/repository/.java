5eb4206295cf3dcb5da121dcb7d8326ac9336993
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 15:27:38 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/fundpolicyservicecontroller/repository/.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/fundpolicyservicecontroller/repository/.java
@@ -1,20 +1,12 @@
 @Repository
-public class FundPolicyServiceControllerRepository {
- 
-    @Autowired
-    private EntityManager entityManager;
- 
-    public CommonResponseDto processPolicy(FundChangeDto fundChangeDto, String serviceId) {
-        FundPolicyService policyService = findPolicyServiceById(serviceId);
-        if (policyService == null) {
-            throw new ApplicationException("Policy service not found")
-                    || new RequiredFieldException("Required field not found");
+public class ProcessMemberPolicyServiceFundPolicyServiceControllerRepository {
+
+    public void processMemberFunds(int sourcePolicyNumber, String effectiveTxnDate, String updateSubType, List<MemberChangeDto> policyMembers) {
+        
+        //Validate the request 
+        if (sourcePolicyNumber <= 0 || StringUtils.isBlank(effectiveTxnDate) || StringUtils.isBlank(updateSubType) || policyMembers.isEmpty()) {
+            throw new IllegalArgumentException("Invalid request");
         }
-        Map<String, String> result = policyService.processPolicy(fundChangeDto);
-        return new CommonResponseDto(result);
-    }
- 
-    public FundPolicyService findPolicyServiceById(String serviceId) {
-        return entityManager.find(FundPolicyService.class, serviceId);
+        // Process the member funds here
     }
 }
