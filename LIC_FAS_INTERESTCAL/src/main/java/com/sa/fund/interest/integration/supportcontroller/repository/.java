3d4ff6e5eaf3d5b10bf0451c96f5a6adc5b6bd95
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-28 15:58:51 +0530

    Updated

--- LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/supportcontroller/repository/.java
+++ LIC_FAS_INTERESTCAL/src/main/java/com/sa/fund/interest/integration/supportcontroller/repository/.java
@@ -1,32 +1,16 @@
 @Repository
-public class SetCreditOpeningByPolicyAndLicIdSupportControllerRepository {
+public class ProcessPolicyServicesSupportControllerRepository {
 
-    @Autowired
-    private PolicyFundCoreService policyFundCoreService;
-    
-    @Autowired
-    private MemberFundCoreService memberFundCoreService;
-    
-    @PostMapping("/setCreditOpeningByPolicyAndLicId")
-    @Operation(summary = "Set Credit Opening for member by Policy Id and Lic Id",
-               description = "{policyId:305,policyNumber:,trnxDate:31/03/2023,isBatch:false,isAuto:true,setOpeningBalance:false,recalculate:true,licId:1}")
-    public CommonResponseDto<Object> setCreditOpeningByPolicyAndLicId(@RequestBody BatchDto batchDto) {
-        try {
-            
-            String policyId = batchDto.getPolicyId();
-            String variant = policyFundCoreService.getVariantForPolicyId(policyId);
-            
-            if (StringUtils.isBlank(variant)) {
-                return new CommonResponseDto<Object>(200, "Variant not found for the policy id: " + policyId, null);
-            }
-            if (variant.equals("V2")) {
-                memberFundCoreService.setOpeningBalanceForNextQuarter(batchDto);
-            } else {
-                memberFundCoreService.setOpeningBalanceForNewFinancialYr(batchDto);
-            }
-            return new CommonResponseDto<Object>(200, "Credit Opening is successfully set for the policy and lic id: " + policyId + " - " + batchDto.getLicId(), null);
-        } catch (Exception ex) {
-            return new CommonResponseDto<Object>(500, "Error in setting the Credit Opening for the policy id: " + batchDto.getPolicyId(), null);
+    public CommonResponseDto processPolicyServices(FundChangeDto fundChangeDto) throws RequiredFieldException, ApplicationException {
+        if (fundChangeDto.getSourcePolicyNumber() == null || fundChangeDto.getDestinationPolicyNumber() == null || fundChangeDto.getUpdateType() == null) {
+            throw new RequiredFieldException("One or more of the required fields are null or empty");
         }
+        if (fundChangeDto.getSourcePolicyNumber() <= 0 || fundChangeDto.getDestinationPolicyNumber() <= 0) {
+            throw new ApplicationException("sourcePolicyNumber or destinationPolicyNumber is not found");
+        }
+        // implement your business logic here 
+        // return a CommonResponseDto with a map containing the result of the processing of the policy service when successful
+        return null;
     }
+
 }
